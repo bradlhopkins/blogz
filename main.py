7d26327a573ce8cb3e8a6b33042aa9f682a22161
Brad Hopkins <bradlhopkins@gmail.com>
2017-10-14 21:48:45 -0500

    functionality stage 1 nearly there.

--- main.py
+++ main.py
@@ -19,32 +19,38 @@ class Blog(db.Model):
     body = db.Column(db.String(5000))
 
     def __init__(self, title, body):
-        self.name = name
+        self.name = title
         self.title = title
         self.body = body
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    #if request.method == 'POST':
+        #blog_title = request.form['title']
+        #blog_post = request.form['body']
+        #new_post = Blog(blog_title, blog_post)
+        #db.session.add(new_post)
+        #db.session.commit()
+    
+
+    blogs = Blog.query.all()
+
+    return render_template('blog.html', title="Hello Blog",
+        blogs=blogs) #, completed_tasks=completed_tasks)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+    
+    if request.method == 'GET':
+        return render_template('newpost.html', title="Hello Blog", subtitle="Add a new blog post.")
+
     if request.method == 'POST':
         blog_title = request.form['title']
-        new_post = Task(task_name)
-        db.session.add(new_task)
+        blog_post = request.form['body']
+        new_post = Blog(blog_title, blog_post)
+        db.session.add(new_post)
         db.session.commit()
-    tasks = Task.query.filter_by(completed=False).all()
-
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html', title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
 
     return redirect('/')
 
