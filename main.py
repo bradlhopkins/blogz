6719606525a48187eed9710ffb2969a5a70f86f2
Brad Hopkins <bradlhopkins@gmail.com>
2017-10-15 11:05:23 -0500

    Ready to work on second and third stages

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Blog(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(5000))
+    body = db.Column(db.String(10000))
 
     def __init__(self, title, body):
         self.name = title
@@ -36,23 +36,44 @@ def index():
 
     blogs = Blog.query.all()
 
-    return render_template('blog.html', title="Hello Blog",
+    return render_template('blog.html', title_main="Hello Blog",
         blogs=blogs) #, completed_tasks=completed_tasks)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
     
+    subtitle = 'Add  a new blog post.'
+    title_main = 'Hello Blog'
+    title_error = ''
+    post_error = ''
+    title = ''
+    body = ''
+    
     if request.method == 'GET':
-        return render_template('newpost.html', title="Hello Blog", subtitle="Add a new blog post.")
+        return render_template('newpost.html', title_main="Hello Blog", subtitle="Add a new blog post.")
 
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_post = request.form['body']
         new_post = Blog(blog_title, blog_post)
+        #db.session.add(new_post)
+        #db.session.commit()
+
+    #Title verification
+    if len(blog_title) == 0:
+        title_error = 'Please enter a title.'
+
+    if len(blog_post) == 0:
+        post_error = 'Please enter a post.'
+
+    if not title_error and not post_error:
         db.session.add(new_post)
         db.session.commit()
 
-    return redirect('/')
+        return redirect('/')
+
+    else:
+        return render_template('newpost.html', title_main=title_main, subtitle=subtitle, title=blog_title, body=blog_post, title_error=title_error, post_error=post_error)
 
 
 if __name__ == '__main__':
